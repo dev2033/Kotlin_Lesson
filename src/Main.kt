ec0cecb775e1620784df1893a648280ef244df1c
USER <dev,elop.02@mail.ru>
2020-09-04 22:14:51 +0300

    new commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+package com.mypackage
+
+//fun main(args: Array<String>) {
+//    """Переменные"""
+//
+//    var age: Int = 10           //var age = 10  не обязательно писать типы данных,
+//                                                // переменная сама принимает на себя,
+//                                                // то значение данных, которые ты задашь
+//    age = 15
+////    println("Переменная " + num1)
+//
+//    val hel: String = "Привет"      //не изменяется(константа)
+//    var name = "Devid"
+//    println("$hel $name, тебе $age лет")
+//}
+
+fun main(args: Array<String>){
+    """Условные операторы"""
+
+    
+}
